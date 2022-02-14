30eddfc3ce63acc24ccb87c9080ec5ac73ca08c0
izioussama1 <izioussama18@gmail.com>
2022-02-14 18:38:24 +0100

    edits

---
+++ details.php
@@ -0,0 +1,24 @@
+<?php
+  require_once("includes/sidebar.php")
+?>
+<body class="bg-light">
+    
+
+  <div class="col-md-10 col-12 bg-light ms-auto">
+    <div class="container-fluid">
+      <div class="d-flex align-items-center pt-3 justify-content-between">
+        <div class="text-dark fw-bold text-uppercase">
+          Details
+        </div>
+        <div class="d-flex align-items-center">
+          <div class="position-relative">
+            <input type="text" placeholder="Search" class="form-control">
+            <i class="ri-search-line position-absolute search-icon"></i>
+          </div>
+          <a href="settings.php" ><i class="ri-settings-2-line ms-3"></i></a>
+          <i class="ri-notification-line ms-3"></i>
+        </div>
+      </div>
+      <hr />
+
+</body>
